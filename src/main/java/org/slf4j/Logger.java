b42aa7a79fd98dc235b289843145fe9d41073226
Rick Horowitz <2948482+rickgittest@users.noreply.github.com>
2018-04-28 07:28:30 -0400

    Added ta4j core files

---
+++ src/main/java/org/slf4j/Logger.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.slf4j;
+
+/**
+ * Replacement of slf4j Logger so we can use jsweet
+ * @author rick
+ *
+ */
+public class Logger {
+
+	public void trace(String msg) {
+	}
+	
+	public void trace(String format,
+      Object arg) {
+	}
+	
+	public void trace(String format,
+      Object arg1,
+      Object arg2) {
+	}
+	
+	public void trace(String format,
+      Object... arguments) {
+	}
+	
+	public void trace(String msg,
+      Throwable t) {
+	}
+	
+}
